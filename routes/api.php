6111878f7e72fc3a354e783d9a97d9c234bd2d5c
lcyitar <lcyitar@gmail.com>
2018-11-05 03:56:40 +0000

    categories index

--- routes/api.php
+++ routes/api.php
@@ -53,6 +53,10 @@ $api->version('v1', [
         $api->delete('authorizations/current', 'AuthorizationsController@destroy')
             ->name('api.authorizations.destroy');
 
+        //帖子分类信息
+        $api->get('categories', 'CategoriesController@index')
+            ->name('api.categories.index');
+
          // 需要 token 验证的接口
         $api->group(['middleware' => 'api.auth'], function($api) {
             // 当前登录用户信息
