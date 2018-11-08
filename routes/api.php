739ceebf22484770657430e85c59d24e075b0543
lcyitar <lcyitar@gmail.com>
2018-11-08 06:03:03 +0000

    notifications stats

--- routes/api.php
+++ routes/api.php
@@ -105,6 +105,10 @@ $api->version('v1', [
             // 通知列表
             $api->get('user/notifications', 'NotificationsController@index')
                 ->name('api.user.notifications.index');
+            //统计通知
+            $api->get('user/notifications/stats', 'NotificationsController@stats')
+                ->name('api.user.notifications.stats');
+
         });
     });
 
