86aa097d227732939e927b47a47690f6c0cc5b83
lcyitar <17607004519@163.com>
2019-04-25 11:17:31 +0000

    travis 测试

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -15,7 +15,7 @@ class HomeController extends Controller
     {
         return Admin::content(function (Content $content) {
 
-            $content->header('larabbs 管理后台');
+            $content->header('lcyitar larabbs 管理后台');
             $content->description('lcyitar larabbs 练习');
 
             $content->row(Dashboard::title());
